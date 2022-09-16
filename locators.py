7b7fb314234e22b3b3a859a3994acf5b0b62f815
Kanokhat Zaidova <zaidovacatherine@yandex.ru>
2022-09-16 14:21:06 +0300

    исправления по ревью

--- locators.py
+++ locators.py
@@ -1,15 +1,18 @@
-"//*[@id=\"root\"]/div/header/nav/a/p" #кнопка личный кабинет
-"//*[@id=\"root\"]/div/main/div/form/button" #кнопка Зарегистрироваться
-"//*[@id=\"root\"]/div/main/div/form/button" #кнопка войти в аккаунт
-"//*[@id=\"root\"]/div/main/div/div/p[2]/a" #кнопка восстановления пароля
-"//*[@id=\"root\"]/div/main/div/nav/ul/li[3]/button" #кнопка выхода
-"//*[@id=\"root\"]/div/header/nav/ul/li[1]/a/p" #конструктор
-"//*[@id=\"root\"]/div/header/nav/div/a" #логотип
-"//*[@id=\"root\"]/div/main/div/form/fieldset[1]/div/div/input" #инпут логина авторизации
-"//*[@id=\"root\"]/div/main/div/form/fieldset[2]/div/div/input" #инпут пароля авторизации
-"//*[@id=\"root\"]/div/main/div/form/fieldset[1]/div/div/input"  #инпут имени для регистрации
-"//*[@id=\"root\"]/div/main/div/form/fieldset[2]/div/div/input"  #инпут логина для регистрации
-"//*[@id=\"root\"]/div/main/div/form/fieldset[3]/div/div/input"  #инпут пароля для регистрации
-"//*[@id=\"root\"]/div/main/section[1]/div[1]/div[3]" #начинки
-"//*[@id=\"root\"]/div/main/section[1]/div[1]/div[2]" #соусы
-"//*[@id=\"root\"]/div/main/section[1]/div[1]/div[1]" #булки
+class Locators():
+    personal_account = ("//*[@id=\"root\"]/div/header/nav/a/p") #кнопка личный кабинет
+    registration_button = ("//*[@id=\"root\"]/div/main/div/div/p[1]/a") #кнопка Зарегистрироваться
+    login_button = ("//*[@id=\"root\"]/div/main/section[2]/div/button") #кнопка войти в аккаунт
+    come_in_button = ("//*[@id=\"root\"]/div/main/div/form/button") #кнопка войти
+    come_in_from_registration_and_reset_password = ("//*[@id=\"root\"]/div/main/div/div/p/a") #кнопка войти со страницы регистрации
+    reset_passwort_button = ("//*[@id=\"root\"]/div/main/div/div/p[2]/a") #кнопка восстановления пароля
+    logout_button = ("//*[@id=\"root\"]/div/main/div/nav/ul/li[3]/button") #кнопка выхода
+    button_in_header_konstruktor = ("//*[@id=\"root\"]/div/header/nav/ul/li[1]/a/p") #конструктор
+    logo = ("//*[@id=\"root\"]/div/header/nav/div/a") #логотип
+    input_login_authorization = ("//*[@id=\"root\"]/div/main/div/form/fieldset[1]/div/div/input") #инпут логина авторизации
+    input_password_authorization = ("//*[@id=\"root\"]/div/main/div/form/fieldset[2]/div/div/input") #инпут пароля авторизации
+    input_name_registration = ("//*[@id=\"root\"]/div/main/div/form/fieldset[1]/div/div/input")  #инпут имени для регистрации
+    input_email_registration = ("//*[@id=\"root\"]/div/main/div/form/fieldset[2]/div/div/input")  #инпут email для регистрации
+    input_password_registration = ("//*[@id=\"root\"]/div/main/div/form/fieldset[3]/div/div/input")  #инпут пароля для регистрации
+    fillings = ("//*[@id=\"root\"]/div/main/section[1]/div[1]/div[3]") #начинки
+    sauce = ("//*[@id=\"root\"]/div/main/section[1]/div[1]/div[2]") #соусы
+    hamburger_buns = ("//*[@id=\"root\"]/div/main/section[1]/div[1]/div[1]") #булки
